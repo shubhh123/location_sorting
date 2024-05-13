1c5b2e9b1f9370fc063f5c15058725654f271ec9
shubhh123 <shubhh1181@gmail.com>
2024-05-13 14:34:32 +0530

    code files committed

--- lib/locationservices.dart
+++ lib/locationservices.dart
@@ -4,13 +4,14 @@ import 'package:geolocator/geolocator.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert'; // For jsonDecode
 import 'package:geocoding/geocoding.dart';
-import 'dart:math';
+
 import 'package:location_sorting/model/address.dart' as customAddress;
 
 import 'package:location_sorting/model/locations.dart';
 
-//Now compute the distance
-//Associate Address Object with Lat and Long
+import 'package:workmanager/workmanager.dart';
+
+const fetchBackground = "fetchBackground";
 
 //ignore: must_be_immutable
 class LocationServices extends StatefulWidget {
@@ -18,8 +19,6 @@ class LocationServices extends StatefulWidget {
 
   double? sourceLattitude;
   double? sourceLongitude;
-
-  //static final Map<double, double> storageLocationLatAndLong = {};
 
   @override
   State<LocationServices> createState() => _LocationServicesState();
@@ -30,6 +29,10 @@ class _LocationServicesState extends State<LocationServices> {
   Map<double, customAddress.Address>
       mapWithStorageAddressAndTheDistanceBetweenUserAndStorageUnit = {};
 
+  List<Location> storageUnitLocations = [];
+
+  List<Locations> storageUnitLatAndLng = [];
+
   @override
   void initState() {
     super.initState();
@@ -38,10 +41,6 @@ class _LocationServicesState extends State<LocationServices> {
 
   mainOperation() async {
     getCurrentLocationOfTheUser();
-
-    List<Location> storageUnitLocations = [];
-
-    List<Locations> storageUnitLatAndLng = [];
 
     List<customAddress.Address> addresses = await constructAddressObject();
 
@@ -53,9 +52,6 @@ class _LocationServicesState extends State<LocationServices> {
       try {
         storageUnitLocations = await getStorageUnitLocationsByForwardGeoCoding(
             address.formattedAddressString);
-
-        //Locations(address.latitude, storageUnitLocations.longitude);
-        //print("Storage Units lat and lng: ${storageUnitLocations.first.latitude} ${storageUnitLocations.first.longitude}");
         Locations newLocation = Locations(storageUnitLocations.first.latitude,
             storageUnitLocations.first.longitude, address);
         storageUnitLatAndLng.add(newLocation);
@@ -63,54 +59,21 @@ class _LocationServicesState extends State<LocationServices> {
         debugPrint(
             "Some of the address fields were not according to the desired format!");
       }
-      //debugPrint("${storageUnitLatAndLng.length}");
     }
 
     for (int i = 0; i < storageUnitLatAndLng.length; i++) {
-      //print("${storageUnitLatAndLng[i].latitude} ${storageUnitLatAndLng[i].longitude} mapped to address ${storageUnitLatAndLng[i].storageAddress.address}");
-
       findDistanceBetweenUserToStorage(widget.sourceLattitude!,
           widget.sourceLongitude!, storageUnitLatAndLng[i]);
     }
-    sortedListWrtKm.sort((a, b) => a.compareTo(b));
-    // for (double value in sortedListWrtKm) {
-    //   debugPrint("Distance from user to storage: $value km");
-    // }
 
-    //Iterate Over the Map..
-    // Sort the map entries based on distance in ascending order\
-
-    var sortedEntries =
-        mapWithStorageAddressAndTheDistanceBetweenUserAndStorageUnit.entries
-            .toList()
-          ..sort((a, b) => a.key.compareTo(b.key));
-
-    // Clear the existing map
-    mapWithStorageAddressAndTheDistanceBetweenUserAndStorageUnit.clear();
-
-    // Reconstruct the map from the sorted entries
-    for (var entry in sortedEntries) {
-      mapWithStorageAddressAndTheDistanceBetweenUserAndStorageUnit[entry.key] =
-          entry.value;
-    }
-
-    // Iterate over the sorted map and print or process as needed
-    for (var entry
-        in mapWithStorageAddressAndTheDistanceBetweenUserAndStorageUnit
-            .entries) {
-      double distance = entry.key;
-      customAddress.Address address = entry.value;
-
-      debugPrint(
-          "Distance: $distance km, Address: ${address.formattedAddressString}");
-    }
+    sortTheMapBasedOnDistance();
   }
 
   //Future<List<Location>>
   Future<List<Location>> getStorageUnitLocationsByForwardGeoCoding(
       String customAddressQuery) async {
     List<Location> locations = await locationFromAddress(customAddressQuery);
-    //debugPrint("Lat and Long obtained from the locationFromAddress method: $locations");
+
     return locations;
   }
 
@@ -124,6 +87,8 @@ class _LocationServicesState extends State<LocationServices> {
       if (jsonObject['data']['admin'].containsKey('Properties')) {
         var properties = jsonObject['data']['admin']['Properties'];
         for (var property in properties) {
+          debugPrint("Property file contents: $property");
+
           if (property.containsKey('Address')) {
             var addressJson =
                 property['Address']; // this is expected to be a map
@@ -170,40 +135,41 @@ class _LocationServicesState extends State<LocationServices> {
 
   double findDistanceBetweenUserToStorage(
       double lat1, double lon1, Locations storageUnitLoc) {
-    // const r = 6372.8; // Earth radius in kilometers
-
-    // //debugPrint("Inside findDistanceBetweenSourceToStorage");
-    // final dLat = _toRadians(storageUnitLoc.latitude - lat1);
-    // final dLon = _toRadians(storageUnitLoc.longitude - lat1);
-    // final lat1Radians = _toRadians(lat1);
-    // final lat2Radians = _toRadians(storageUnitLoc.latitude);
-
-    // final a =
-    //     _haversin(dLat) + cos(lat1Radians) * cos(lat2Radians) * _haversin(dLon);
-    // final c = 2 * asin(sqrt(a));
-
-    // double distance = r * c;
-
-    // //debugPrint("Distance between user to storage unit: ${r * c} km");
-    // sortedListWrtKm.add(r * c);
-
-    // mapWithStorageAddressAndTheDistanceBetweenUserAndStorageUnit[distance] =
-    //     storageUnitLoc.storageAddress;
-
-    // debugPrint("\n");
-    // return distance;
     FlutterMapMath flutterMapMath = FlutterMapMath();
     double distance = flutterMapMath.distanceBetween(lat1, lon1,
         storageUnitLoc.latitude, storageUnitLoc.longitude, "kilometers");
 
     mapWithStorageAddressAndTheDistanceBetweenUserAndStorageUnit[distance] =
         storageUnitLoc.storageAddress;
-
     return distance;
   }
 
-  double _toRadians(double degrees) => degrees * (pi / 180);
-  double _haversin(double radians) => pow(sin(radians / 2), 2) as double;
+  void sortTheMapBasedOnDistance() {
+    var sortedEntries =
+        mapWithStorageAddressAndTheDistanceBetweenUserAndStorageUnit.entries
+            .toList()
+          ..sort((a, b) => a.key.compareTo(b.key));
+
+    // Clear the existing map
+    mapWithStorageAddressAndTheDistanceBetweenUserAndStorageUnit.clear();
+
+    // Reconstruct the map from the sorted entries
+    for (var entry in sortedEntries) {
+      mapWithStorageAddressAndTheDistanceBetweenUserAndStorageUnit[entry.key] =
+          entry.value;
+    }
+
+    // Iterate over the sorted map and print or process as needed
+    for (var entry
+        in mapWithStorageAddressAndTheDistanceBetweenUserAndStorageUnit
+            .entries) {
+      double distance = entry.key;
+      customAddress.Address address = entry.value;
+
+      debugPrint(
+          "Distance: $distance km, Address: ${address.formattedAddressString}");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
